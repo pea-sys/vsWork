3b8d21a6e8737294998f88262795461b15864bf6
pea-sys <pea98258@gmail.com>
2021-06-15 22:33:45 +0900

    Clockコンポーネントのテストをお試しで追加 引き続きテストの追加を行っていく予定

---
+++ vsWork.Test/ClockTest.cs
@@ -0,0 +1,68 @@
+﻿using Bunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vsWork.Shared;
+using Xunit;
+
+namespace vsWork.Test
+{
+    public class ClockTest
+    {
+        string fontSize = "10";
+        string stringColor = "#0000ff";
+
+        [Fact(DisplayName = "パラメータセットなしで表示可能なこと")]
+        public void NoneParameter()
+        {
+            using var ctx = new TestContext();
+            var component = ctx.RenderComponent<Clock>();
+            component.SetParametersAndRender(parameters => parameters.Add(p => p.FontSize, string.Empty));
+        }
+
+        [Fact(DisplayName = "日時がリアルタイム表示されること")]
+        public void RealTimeDisp()
+        {
+            using var ctx = new TestContext();
+            var component = ctx.RenderComponent<Clock>();
+            component.SetParametersAndRender(parameters => parameters.Add(p => p.FontSize, fontSize));
+
+            var oldFontElmText = DateTime.Parse(component.Find("font").TextContent);
+            System.Threading.Thread.Sleep(2000);
+            var newFontElmText = DateTime.Parse(component.Find("font").TextContent);
+            TimeSpan ts = newFontElmText - oldFontElmText;
+            // 正確な時差は取得できる保証がないので時間が進んでいたらOK
+            Assert.True(ts.TotalSeconds > 0);
+        }
+
+        [Fact(DisplayName = "FontSizeパラメータが有効であること")]
+        public void ParameterFontSize()
+        {
+            using var ctx = new TestContext();
+            var component = ctx.RenderComponent<Clock>();
+            fontSize = "1";
+            component.SetParametersAndRender(parameters => parameters.Add(p => p.FontSize, fontSize));
+            Assert.True(component.Find("font").GetAttribute("size") == $"{fontSize}px");
+
+            fontSize = "99";
+            component.SetParametersAndRender(parameters => parameters.Add(p => p.FontSize, fontSize));
+            Assert.True(component.Find("font").GetAttribute("size") == $"{fontSize}px");
+        }
+        [Fact(DisplayName = "StringColorパラメータが有効であること")]
+        public void ParameterStringColor()
+        {
+            using var ctx = new TestContext();
+            var component = ctx.RenderComponent<Clock>();
+            stringColor = "Red";
+            component.SetParametersAndRender(parameters => parameters.Add(p => p.StringColor, stringColor));
+            Assert.True(component.Find("font").GetAttribute("color") == $"{stringColor}");
+
+            stringColor = "#0000ff";
+            component.SetParametersAndRender(parameters => parameters.Add(p => p.StringColor, stringColor));
+            Assert.True(component.Find("font").GetAttribute("color") == $"{stringColor}");
+        }
+
+    }
+}
